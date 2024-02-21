60aa5e6aebb5bdda8cc0394388bbedf1f60d5d51
klupsky <katharina@chalupsky.eu>
2024-02-21 17:39:28 +0100

    recommendation functionality works

--- app/(pages)/recommendation/[...index]/page.tsx
+++ app/(pages)/recommendation/[...index]/page.tsx
@@ -1,4 +1,5 @@
 import Recommendation from '../../../components/Recommendation/clientComponent';
+import { getRecommendationBasedOnUrlAndDatabase } from '../../../lib/data';
 
 interface TParams {
   params: {
@@ -7,18 +8,21 @@ interface TParams {
 }
 
 export default async function Page({ params }: TParams) {
-  console.log(params.index);
   const encodedParams = params.index[0];
   const decodedParams = encodedParams.replace(/%3D/g, '=').replace(/%26/g, '&');
-  console.log(decodedParams, 'decodedParams');
   const paramsArray = decodedParams.split('&');
   const [flavour, spirit, level] = paramsArray.map(
     (param: string) => param.split('=')[1],
   );
 
+  const recommedation = await getRecommendationBasedOnUrlAndDatabase(
+    flavour,
+    spirit,
+    level,
+  );
+  console.log(recommedation);
   return (
     <main>
-      hi
       <Recommendation />
     </main>
   );
