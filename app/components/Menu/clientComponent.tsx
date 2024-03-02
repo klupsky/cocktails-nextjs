c956933bd90520a1a102fa9c92b276fee5a7db55
klupsky <katharina@chalupsky.eu>
2024-03-02 13:11:43 +0100

    menu is styled

--- app/components/Menu/clientComponent.tsx
+++ app/components/Menu/clientComponent.tsx
@@ -20,7 +20,7 @@ const Menu = ({ user, handleSignOut }: TMenuProps) => {
     <div className="width-full relative">
       <button
         onClick={toggleMenu}
-        className="absolute right-12 top-12 z-10 flex flex-col items-center justify-center"
+        className="absolute right-12 top-12 z-20 flex flex-col items-center justify-center"
       >
         <span
           className={`block h-0.5 w-6 rounded-sm bg-black transition-all duration-300 ease-out ${
@@ -40,25 +40,60 @@ const Menu = ({ user, handleSignOut }: TMenuProps) => {
       </button>
 
       <div
-        className={`z-2 fixed top-0 h-[100vh] w-full bg-red transition-all duration-300 ease-out ${
+        className={`fixed top-0 z-10 flex h-[100vh] w-full flex-col items-center justify-between bg-red p-12 transition-all duration-300 ease-out ${
           menuOpen ? 'translate-x-0' : 'translate-x-full'
         }`}
+        onClick={toggleMenu}
       >
-        {user ? (
-          <form
-            onSubmit={async (e) => {
-              e.preventDefault();
-              await handleSignOut();
-              setMenuOpen(false);
-            }}
-          >
-            <button type="submit">
-              <div>Sign Out</div>
-            </button>
-          </form>
-        ) : (
-          <Link href="/login">Sign In</Link>
-        )}
+        <div className="max-w-[120px] text-center">
+          <Link href="/">
+            <h3 className="headline-md">Fancy a cocktail?</h3>
+          </Link>
+        </div>
+
+        <ul className="headline-lg flex flex-col gap-4 text-center">
+          <li>
+            <Link href="/recommendation">Find a cocktail</Link>
+          </li>
+
+          <li>
+            <Link href="/collection">Full Collection</Link>
+          </li>
+          {user && (
+            <li>
+              <Link href="/dashboard">Your selection</Link>
+            </li>
+          )}
+        </ul>
+
+        <ul className="flex gap-8">
+          {user ? (
+            <form
+              onSubmit={async (e) => {
+                e.preventDefault();
+                await handleSignOut();
+                setMenuOpen(false);
+              }}
+            >
+              <button type="submit">
+                <li>Sign Out</li>
+              </button>
+            </form>
+          ) : (
+            <>
+              <li>
+                <Link href="/login">Sign In</Link>
+              </li>
+              <li>
+                <Link href="/register">Register</Link>
+              </li>
+            </>
+          )}
+
+          <li>
+            <Link href="/imprint">imprint</Link>
+          </li>
+        </ul>
       </div>
     </div>
   );
